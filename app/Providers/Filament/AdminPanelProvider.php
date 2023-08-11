da826007808c6ad0695b5f4865ba88d83e650ef4
Debjit Biswas <debjit012@gmail.com>
2023-08-11 21:51:59 +0530

    wip

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -27,6 +27,7 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             // ->registration()
+            // ->topNavigation()
             ->login()
             ->colors([
                 'primary' => Color::Amber,
