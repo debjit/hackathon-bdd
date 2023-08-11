0311abc49015953a7fd024810fbf6dd37aa0b1e3
Debjit Biswas <debjit012@gmail.com>
2023-08-12 01:30:47 +0530

    updated_boot_reccord_for_https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,6 +21,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (env('APP_ENV') === 'production') {
+            URL::forceSchema('https');
+        }
         Model::unguard();
     }
 }
